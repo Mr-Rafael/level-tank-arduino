7f606bb7da35df4fbdd781dab09e768fee5af871
unknown <rafael.mazariegosr@gmail.com>
2018-04-30 08:43:18 -0600

    First commit

---
+++ Generator/Generator.ino
@@ -0,0 +1,142 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+
+// Identificación de la red local
+char* ssid = "ECOLGB";
+char* password = "ecolgb123";
+IPAddress ip(192,168,2,101);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& generatorJson = jsonBuffer.createObject();
+JsonObject& batteryJson = jsonBuffer.createObject();                                //CAMBIO 8: se creó otro jsonObject para el envío de lecturas del voltaje de batería.
+
+ int generatorPIN = D0;                                                              //CAMBIO 1: ahora se lee el encendido del MG usando el pin digital D0 en lugar del analógico A0.
+ int batteryPIN = A0;                                                                //CAMBIO 2: ahora se utiliza el pin A0 para leer el voltaje de la batería de MG.
+ int analogData;
+ boolean generatorRunning;
+ int statusChange = false;
+ float batteryVoltage;
+ boolean generatorState = false; 
+ String meterId = "11"; // INGRESAR METER ID                                                                          //CAMBIO 7: se agregó un timer, para que se envíen lecturas de batería cada 5 segundos.
+ 
+void setup() {
+  pinMode(batteryPIN, INPUT);
+  WiFi.begin(ssid,password);   // Inicio de sesión
+  WiFi.config(ip, gateway, subnet);
+  Serial.begin(115200);       // Rate usada por el modulo
+  Serial.println("PROBANDO");
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("conectando...");
+  }
+ Serial.println("ID: ");
+ Serial.println(meterId);
+ //t.every(5000, sendBattery);                                                //CAMBIO 8: la rutina sendBattery se repite cada 5 segundos
+}
+ 
+void loop() {
+   generatorRunning = digitalRead(generatorPIN);                            //CAMBIO 3: se hace digitalRead del pin D0 (encendido del MG).se cambió el nombre de la variable a generatorRunning
+   analogData = analogRead(batteryPIN);                                     //CAMBIO 4: se hace analogRead del pin A0 (voltaje de batería).
+   batteryVoltage = analogData * (3.3/1023.0) * (13.3/3.3);             //CAMBIO 5: se calcula el voltaje en A0. Luego, se calcula el voltaje de batería usando el divisor de voltaje.
+   Serial.println(batteryVoltage);
+   Serial.println(batteryVoltage);
+   if(generatorRunning){  //CMOS                                              CAMBIO 6: se quitó la comparación de voltaje que existía en el IF por una verificación lógica.
+      generatorState = true;
+   }
+   else{
+      generatorState = false;
+   }
+   
+   Serial.print("MOTOR: ");
+   Serial.println(generatorState);
+   Serial.print("VOLTAGE: ");
+   Serial.println(batteryVoltage);
+   sendBattery();
+  delay(5000);
+}
+
+// ---------------------------- esta parte es vestigial, se usaba para mandar datos CUANDO se apagara o encendiera el MG --------------------
+
+/*void generatorStateON(){
+
+    HTTPClient http;    //Declare object of class HTTPClient
+    
+    //Datos de Cliente set-Motor
+    http.begin("http://192.168.2.221:4000/set-motor");      //Specify request destination
+    http.addHeader("Content-Type", "application/json");  //Specify content-type header
+    
+    //Creando JSON GENERATORDATA
+    generatorJson["is_on"] = true;
+    generatorJson["meter_id"] = meterId;
+    String generatorData;
+    generatorJson.printTo(generatorData); 
+    int httpCode = http.POST(generatorData);   //Send the request
+    String payload = http.getString();                  //Get the response payload
+    
+    //Datos en Pantalla
+    Serial.print("RESPUESTA DEL SERVIDOR: ");
+    Serial.println(payload);    //Print request response payload
+    http.end();  //Close connection;   
+    
+   
+
+
+}
+void generatorStateOFF(){
+
+     HTTPClient http;    //Declare object of class HTTPClient
+  
+     //Datos de Cliente set-Motor
+     http.begin("http://192.168.2.221:4000/set-motor");      //Specify request destination
+     http.addHeader("Content-Type", "application/json");  //Specify content-type header
+     
+     //Creando JSON GENERATORDATA
+     generatorJson["is_on"] = false;
+     generatorJson["meter_id"] = meterId;
+     String generatorData;
+     generatorJson.printTo(generatorData); 
+     int httpCode = http.POST(generatorData);   //Send the request
+     String payload = http.getString();                  //Get the response payload
+  
+     //Datos en Pantalla
+     Serial.print("RESPUESTA DEL SERVIDOR: ");
+     Serial.println(payload);    //Print request response payload
+     http.end();  //Close connection;    
+  
+  
+}
+
+*/
+
+void sendBattery(){                                        
+
+    
+     //CAMBIO 9: se creó una rutina que ocurre cada 2 segundos, que envía la lectura de batería.
+     HTTPClient http;    //Declare object of class HTTPClient
+     Serial.println("test--------------------------------------------");
+     //Datos de Cliente set-Motor
+     http.begin("http://192.168.2.221:4000/send-voltage-battery");      //Specify request destination
+     http.addHeader("Content-Type", "application/json");  //Specify content-type header
+     
+     //Creando JSON BATTERYDATA
+     batteryJson["battery_voltage"] = (float) batteryVoltage;
+     batteryJson["is_on"] = generatorState;
+     batteryJson["meter_id"] = meterId;
+     String batteryData;
+     batteryJson.printTo(batteryData); 
+     Serial.println(batteryData);
+     int httpCode = http.POST(batteryData);   //Send the request
+     String payload = http.getString();                  //Get the response payload
+  
+     //Datos en Pantalla
+     Serial.print("RESPUESTA DEL SERVIDOR: ");
+     Serial.println(payload);    //Print request response payload
+     http.end();  //Close connection;       
+  
+}
---
+++ webServerUS100Rem/webServerUS100Rem.ino
@@ -0,0 +1,244 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include <math.h>
+#include "ESP8266HTTPClient.h"
+#include <SoftwareSerial.h>
+#include <EEPROM.h>
+
+const float pi = 3.1416;
+//Definir los pines del SensorUltrasonico
+#define RX  4
+#define TX  5
+
+float DIAMETER = /*147.32 */ 56.5;
+float LENGTH = /*182.88 */ 86.5;
+int MAXVOLUME = 823;
+
+struct datapoint {
+   float distance;
+   float volume;
+};
+
+/*-------------------------- Struct de la EEPROM. Se usa para guardar las dimensiones del tanque, y que sobrevivan apagones. -----------------------------------------------*/
+struct EEprom_package
+{
+    float l;
+    float d;
+    bool s;
+} EEprom_Data;
+
+
+// Variables Globales
+  float sonnarValue = 0;
+  float sonnarHeight = 0;
+  float temperatureValue = 0;
+
+char*  meterId = "9"; // INGRESAR METER ID
+
+struct datapoint data;
+
+ESP8266WebServer server;
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+JsonObject& levelJson = jsonBuffer.createObject();
+
+// Identificación de la red local
+
+char* ssid = "ECOLGB";
+char* password = "ecolgb123";
+IPAddress ip(192,168,2,102);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+//-------------------------------------
+
+// Comunicacion serial con el sensor.
+SoftwareSerial sensorSerial(RX,TX);
+
+void setup()
+{
+  // Inicio de sesión
+  WiFi.begin(ssid,password);   
+  WiFi.config(ip, gateway, subnet);
+
+  //Comunicacion serial con PC y con Sensor
+  Serial.begin(9600);
+  sensorSerial.begin(9600);
+
+  
+  while(WiFi.status()!= WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+  EEPROMLoad();
+  
+  server.on("/",[](){server.send(200,"text/plain","INDEX");});    // raìz
+  server.on("/gas-level",gaslevel);                               // gas-level ruta
+  server.on("/configData", HTTP_POST, [](){                       // configData Seteo de Valores desde POST
+    Serial.println("Cambiando Dimensiones del Tanque");
+    StaticJsonBuffer<200> configBuffer;
+    JsonObject& configJson= configBuffer.parseObject(server.arg("plain"));
+    String l = configJson["l"];
+    String d = configJson["d"];
+    String v = configJson["vol"];
+    String meterId2 = configJson["meterId"];
+    String volumenTotal = configJson["vol"];
+    Serial.println("-------------UPDATING TANK -------------");
+    DIAMETER = d.toFloat();                                //Las medidas del smart site se envían en pulgadas, aquí se convierten a cm.
+    LENGTH = l.toFloat();
+    MAXVOLUME = v.toInt();
+    server.send ( 200, "text/json", "{success:true}" );
+    EEPROMSave();
+    EEPROMLoad();
+  });  
+  server.begin();   // Iniciar el servidor
+  delay(10000);
+}
+
+void loop() 
+{
+  server.handleClient();  //Escuchar
+  data = leerSonnar();
+  sendGasLevel(data.volume, data.distance);
+  delay(2000);
+}
+
+void gaslevel()       // GET para gas-level 
+{
+  
+  root["sonnar"] = sonnarValue;         //Sonnar value volume in gals
+  root["temperature"] = temperatureValue;      //Temperature value
+  root["meterId"] = meterId;      //Temperature value\
+  
+  String gasLevelJson;     
+  root.printTo(gasLevelJson); // Formato
+  
+  server.send(200,"text/json",gasLevelJson); // send
+   
+}
+
+void sendGasLevel(float sonnarValue, float sonnarHeight){
+  if(WiFi.status()== WL_CONNECTED){
+      Serial.println("Sending gas level..");
+     HTTPClient http;    //Declare object of class HTTPClient
+     //Datos de Cliente set-Motor
+     http.begin("http://192.168.2.221:4000/send-gas-level");      //Specify request destination
+     http.addHeader("Content-Type", "application/json");  //Specify content-type header
+     
+     //Creando JSON GENERATORDATA
+     levelJson["level"] = sonnarValue;
+     levelJson["distance"] = sonnarHeight;
+     String levelData;
+     levelJson.printTo(levelData); 
+     int httpCode = http.POST(levelData);   //Send the request
+     String payload = http.getString();         //Get the response payload
+  
+     /*
+     Datos en Pantalla
+     Serial.print("RESPUESTA DEL SERVIDOR: ");
+     Serial.println(payload);    /Print request response payload */
+     http.end();  //Close connection;    
+  }
+}
+  
+struct datapoint leerSonnar(){
+  Serial.println("____________________________________________________________");
+  struct datapoint res;
+  float valorH;
+  float height = DIAMETER;
+  float len = LENGTH;
+  float radius = height/2.0;
+  float volume;
+
+  valorH = measureDistance();
+  volume = calcVol(valorH , radius, len);          // Se calcula el volumen de combustible, ya convertido a galones.
+  Serial.println("Volumen calculado, en galones:");
+  Serial.println(volume);
+  /*volume = volume * 14.39;   */                              //simulacion de laboratorio
+  res.distance = valorH;
+  Serial.println("Distancia al Combustible (cm): ");
+  Serial.println(valorH);
+  Serial.println("Valor Calculado (gal): ");
+  Serial.println(volume);
+  res.volume = volume;
+  return res;  
+}
+
+float measureDistance() {
+  unsigned int mSByte;
+  unsigned int lSByte;
+  float distance;
+  int temp;
+
+  sensorSerial.flush();
+  sensorSerial.write(0x55);                     //Solicitud de medicion de distancia
+  delay(500);
+
+  if (sensorSerial.available() >= 2) {
+    mSByte = sensorSerial.read();
+    lSByte = sensorSerial.read();
+    distance = mSByte * 256 + lSByte;
+  } else {
+    Serial.println("Sensor no respondio solicitud de distancia");
+    distance = 0;
+  }
+
+  sensorSerial.flush();
+  sensorSerial.write(0x50);                    //Solicitud de medicion de temperatura
+
+  delay(500);
+
+  if (sensorSerial.available() >= 1) {
+                                                      Serial.println("Recibido temp");
+    temp = sensorSerial.read();
+    temp -= 45;
+  } else {
+    Serial.println("Sensor no respondio solicitud de temperatura");
+    temp = 0;
+  }
+
+  Serial.print(temp, DEC);
+  Serial.println(" °C");
+  
+  return (distance/10.0);
+}
+ 
+float calcVol(float h, float r, float l) {
+  Serial.print("Altura actual: ");
+  Serial.println(DIAMETER);
+  Serial.print("Longitud actual: ");
+  Serial.println(LENGTH);
+  float vol;
+  if (h <= (2*r) && h >= 0) {
+    vol = (pi*r*r - r*r*acos((r-h)/r) + (r-h)*sqrt(2*r*h-h*h))*l* 0.000264172;
+  } else {
+    vol = 0.001;
+  }
+  return vol;
+}
+
+void EEPROMSave() {
+    EEprom_Data.l = LENGTH;
+    EEprom_Data.d = DIAMETER;
+    Serial.println("Guardado en Memoria:");
+    Serial.println(EEprom_Data.l);
+    Serial.println(EEprom_Data.d);
+    EEPROM.begin(512);
+    EEPROM.put(0, EEprom_Data);
+    EEPROM.end();
+    EEPROM.begin(512);
+    EEPROM.put(0, EEprom_Data);
+    EEPROM.end();
+}
+
+void EEPROMLoad() {
+  EEPROM.begin(512);
+  EEPROM.get(0,EEprom_Data);
+  EEPROM.commit();
+  EEPROM.end();
+  DIAMETER=EEprom_Data.d;
+  LENGTH=EEprom_Data.l;
+}
+
